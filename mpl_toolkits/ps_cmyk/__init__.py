a085c62961e9ac64c95d43e9894724e54d02c865
Jae-Joon Lee <lee.j.joon@gmail.com>
2010-01-17 20:38:10 -0500

    register tight_bbox_handler

--- mpl_toolkits/ps_cmyk/__init__.py
+++ mpl_toolkits/ps_cmyk/__init__.py
@@ -1,10 +1,16 @@
 
 
-import matplotlib.backend_bases
-from backend_ps_cmyk import FigureCanvasPSCMYK
 
-matplotlib.backend_bases.register_backend("eps_cmyk", FigureCanvasPSCMYK)
-matplotlib.backend_bases.register_backend("ps_cmyk", FigureCanvasPSCMYK)
+def _init():
+    import matplotlib.backend_bases
+    from backend_ps_cmyk import FigureCanvasPSCMYK
+    import matplotlib.tight_bbox as tight_bbox
+
+    for format in ["eps_cmyk", "ps_cmyk"]:
+        matplotlib.backend_bases.register_backend(format, FigureCanvasPSCMYK)
+        tight_bbox._adjust_bbox_handler_d[format] = tight_bbox.adjust_bbox_pdf
+
+_init()
 
 def use_profiles(rgb_profile_name, cmyk_profile_name):
     from backend_ps_cmyk import RendererPSCMYK
